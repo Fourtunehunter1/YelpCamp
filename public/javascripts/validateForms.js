6c6b7b69e08103099bd56ed94d195627f2d0ec7f
Patrick Gannon <gannon.patrick01@gmail.com>
2021-02-18 17:48:33 -0500

    Limit images to only be images

--- public/javascripts/validateForms.js
+++ public/javascripts/validateForms.js
@@ -4,7 +4,23 @@
     bsCustomFileInput.init()
 
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.validated-form')
+    const forms = document.querySelectorAll('.validated-form');
+
+    const image = document.getElementById("image");
+
+    image.onchange = function(e) {
+        let ext = this.value.match(/\.([^\.]+)$/)[1];
+        switch(ext) {
+            case 'jpg':
+            case 'bmp':
+            case 'png':
+            case 'jpeg':
+                break; 
+            default:
+                alert("Incorrect Format, file must be an image!");
+                this.value = "";
+        }
+    };
 
     // Loop over them and prevent submission
     Array.from(forms)
